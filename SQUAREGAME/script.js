9c64e87841e7c1b73f84bb69379dee323d37b5d7
VIVEK <vivekkumarsharma2663@gmail.com>
2024-11-24 16:48:14 +0530

    Add files via upload

---
+++ SQUAREGAME/script.js
@@ -0,0 +1,14 @@
+let main=document.getElementById("main");
+let arr=
+["https://i.pinimg.com/control2/236x/5d/6d/23/5d6d23fd7adb44baba20a60c252da339.jpg",
+"https://i.pinimg.com/control2/236x/4c/a0/57/4ca057215fc1e2c9950b36797603e0a8.jpg",
+"https://i.pinimg.com/control2/736x/57/b6/87/57b6875556ec04d11e3327645707b043.jpg",
+"https://i.pinimg.com/control2/236x/42/2b/9e/422b9e1d9153af11a5a5164a41762d4c.jpg",
+"https://i.pinimg.com/control2/236x/5d/81/4a/5d814a02674d5079375cf00515a8183e.jpg",
+"https://i.pinimg.com/control2/236x/00/59/28/005928bfff14447ce471e2aff83b7988.jpg"];
+let s="";
+for(let i=1;i<=65;i++){
+    let r=Math.floor(Math.random()*6);
+    s+=`<div class="card"><img src=${arr[r]}></div>`;
+}
+main.innerHTML=s;
